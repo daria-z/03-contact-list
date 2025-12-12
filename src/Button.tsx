20dd6ffbdf637a0bebc15769099fa58e31a99ef9
daria.daria <33233023+daria-z@users.noreply.github.com>
2025-12-12 22:52:29 +0500

    form with react-hook-form

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,19 +1,20 @@
 import type React from "react"
 
 interface ButtonProps {
-  children: React.ReactNode;
-  onClick?: () => void;
-  disabled?: boolean;
-  variant?: "primary" | "danger" | "secondary"
+  children: React.ReactNode,
+  onClick?: () => void,
+  disabled?: boolean,
+  variant?: "primary" | "danger" | "secondary",
+  type?: "button" | "submit" | "reset" | undefined
 }
 
 
-export const Button = ({ children, onClick, disabled = false, variant = "primary" }: ButtonProps) => {
+export const Button = ({ children, onClick, disabled = false, variant = "primary", type = "button" }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      type="button"
+      type={type}
       className={`button button--${variant}`}
     >
       {children}
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,19 +1,46 @@
+import { useContactsStore } from "./store";
+import { useForm } from "react-hook-form";
 import { Button } from "./Button";
 
+interface FormInput {
+  name: string,
+  position: string,
+  phone: string
+}
+
 export const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInput>();
+  const addContact = useContactsStore((state) => state.addContact);
+  const clearAllContacts = useContactsStore((state) => state.clearAllContacts);
+  const onSubmit = (data: FormInput) => {
+    addContact(data);
+    console.log(data);
+  };
+
+
   return (
     <>
-      <form>
-        <input type="text" name="name" id="name" />
-        <input type="text" name="position" id="position" />
-        <input type="text" name="phone" id="phone" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Name</label>
+        <input {...register("name", { required: true })} placeholder="Name" />
+        {errors.name && <p>Name is required.</p>}
+        <label>Position</label>
+        <input
+          {...register("position", { required: true })}
+          placeholder="Position"
+        />
+        <label>Phone Number</label>
+        <input {...register("phone", { required: true })} placeholder="Phone" />
+        <Button type="submit">Add</Button>
       </form>
-      <Button>Add</Button>
-      <Button variant="secondary">Clear All</Button>
+      <Button variant="secondary" onClick={clearAllContacts}>
+        Clear All
+      </Button>
       <Button variant="secondary">Search</Button>
     </>
   );
-}
-
-
-
+};
